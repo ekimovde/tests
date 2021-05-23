1354962476ed08dfbd04433c0bdeff1619bfaa4d
ekimovde <ekimov_de@mail.ru>
2021-05-23 19:39:13 +0700

    Update base page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { connect } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import { Main, Auth } from "layouts";
+
+const App = ({ token }) => {
+  let isAuth = !!token;
+
+  let routes = (
+    <Switch>
+      <Route path="/auth" component={Auth} exact />
+      <Redirect to="/auth" />
+    </Switch>
   );
-}
 
-export default App;
+  if (isAuth) {
+    routes = (
+      <Switch>
+        <Route path="/" component={Main} exact />
+        <Route
+          path="/project/:projectId/project-structure​/:structureId"
+          component={Main}
+          exact
+        />
+        <Redirect to="/" />
+      </Switch>
+    );
+  }
+
+  return <div className="app">{routes}</div>;
+};
+
+export default connect(({ auth }) => ({ token: auth.token }))(App);
